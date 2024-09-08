c0d8f573b2452b485765a79b3a8b639937528ede
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-09-08 20:59:54 +0330

    Add files via upload

---
+++ JOptionpane.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class JOptionpane {
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null,"welcome to java",
+                                                                    "java",
+                                                                     javax.swing.JOptionPane.ERROR_MESSAGE);
+    }
+}
