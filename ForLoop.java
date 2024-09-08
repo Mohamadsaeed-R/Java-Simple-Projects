53164766ef09fd6dde55d32710af8082e148b2f9
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-09-08 20:55:17 +0330

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i =0;i<10;i++) {
+            System.out.format("%d\t%d\n",i,i*i); //calculates second exponent of the numbers
+        }
+    }
+}
+
