5562ee4a841d7997d3e008f68730a9ba25769d74
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-11-11 18:47:14 +0330

    Update JOptionpane3.java

--- JOptionpane3.java
+++ JOptionpane3.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class JOptionpane3 {
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Enter a number");
-        int n = Integer.parseInt(name);
+        int n = Integer.parseInt(name);                //This part of the code calls the parseInt method from the Integer class. This method converts a string (String) into an integer (int).
         String message;
         if(n%2==0){
             message="this is an even number";
