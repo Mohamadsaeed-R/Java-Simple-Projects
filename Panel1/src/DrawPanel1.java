8c037317f13fa8dc3c8adf69b6bcedc887da3f14
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-11-12 19:29:07 +0330

    Add files via upload

---
+++ Panel1/src/DrawPanel1.java
@@ -0,0 +1,5 @@
+public class DrawPanel1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Panel1/src/MyPanel.java
@@ -0,0 +1,10 @@
+import java.awt.*;      //this library includes classes for creating graphical user interfaces
+import javax.swing.JPanel;
+public class MyPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+     super.paintComponent(g);
+    }
+
+
+
+}
