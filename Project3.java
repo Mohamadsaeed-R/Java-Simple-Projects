2d89b5e933bcdfe105ce77bdb8bcbc9e4211db8b
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-03-28 18:04:51 +0330

    Update Project3.java

--- Project3.java
+++ Project3.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Project3 {
         public static void main(String[]args){
-            Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);// ساخت شیء Scanner
             System.out.println("please enter an integer:\n");
             int n=input.nextInt() ;
             String Type=(n % 2 == 0)? "Even":"odd";
