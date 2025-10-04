b4767666cf22cae1f6d498f1d8a241292c27cece
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-04 19:35:27 +0330

    Update DivisorsOfANumber.java

--- DivisorsOfANumber.java
+++ DivisorsOfANumber.java
@@ -1,30 +1,21 @@
-//The divisors of a number refer to the numbers that can divide that number without leaving a remainder.
- //A prime number has only two divisors
-
-import java.util.*;
-public class DivisorsOfANumber {
-    public static void main(String[]args){
-        Scanner input=new Scanner (System.in);
-        System.out.println("Enter an integer:") ;
-        int n=input.nextInt();
-            System.out.format("Divisors of a number %d\n",n);
-        int d = 0;
-        for(int i = 1 ; i<=n ; i++){
-            if (n % i==0) //this condition is used in the code to find all divisors of the number n.  ------------------//if (i % 2 == 0): this condition checks whether the number i is even.
-            //If n % i == 0, it means that n is divisible by i."
-            {
-                System.out.format("%d\n",i);
+import java.util.Scanner;
+public class newapp {
+    public static void main(String[] ans) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a number:\n"); //جای این خط مهمه
+        int n  = input.nextInt();
+        int d= 0;
+        for (int i = 1; i  <= n; i++) {            //int i = 1 یعنی از عدد ۱ شروع کن
+                                                    //مهمه که حتمااینجوری بنویسم تا خود عدد رو هم شامل بشه تا کامل باشه i<= n
+            if (n % i == 0) {                     //در واقع داره تمام عددهای ۱ تا n رو یکی‌یکی امتحان می‌کنه
+                                                     //تا ببینه کدوم‌ها مقسوم‌علیه n هستن.
+                System.out.format("%d\t\n", i);
                 d++;
             }
-        }
-
-        System.out.format("Divisors of the number %d:\n",d);
-        if (d==2) //A prime number has exactly two divisors{
-            System.out.format("%d is a prime number \n ",n);
-        }
-        else {
-            System.out.format("%d is not a prime number \n ",n);
-        }
-
+        }System.out.format("number of divisors is: %d\n", d); //شمارش d باید بعد از اتمام حلقه چاپ بشه، نه داخل حلقه.
     }
 }
+// دلیل این که چرا چاپ d باید بعد از حلقه باشه
+//حلقه در حال شمارش است
+//در هر دور از حلقه، متغیر d به تعداد مقسوم‌علیه‌های عدد n اضافه می‌شود.
+//اگر شما مقدار d رو داخل حلقه چاپ کنی، در هر تکرار حلقه، مقدار d نمایش داده می‌شود که این باعث ایجاد خروجی اضافی می‌شود.
