eb922c9c9934ab8119b8faba874f13a464c85807
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-08 17:25:08 +0330

    Update JOptionpane.java

--- JOptionpane.java
+++ JOptionpane.java
@@ -3,6 +3,8 @@ public class JOptionpane {
     public static void main(String[] args) {
         javax.swing.JOptionPane.showMessageDialog(null,"welcome to java",
                 "java",
-                JOptionPane.ERROR_MESSAGE);
+                JOptionPane.ERROR_MESSAGE);              // JOptionPane.showMessageDialog(null,"Hello World","My first app",
+                                                                        JOptionPane.WARNING_MESSAGE);
     }
 }
+
