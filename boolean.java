4f237c851838b9cba5c6544b8295ab79522f42dd
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-17 15:20:40 +0000

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,9 @@
+public class app {
+    public static void main(String []args){
+        System.out.println("Hello World");
+        boolean isAlien =false;//یک شرط منطقی را در خود قرار میدهد
+        if (!isAlien){
+            System.out.println("its not an alien!");
+        }
+    }
+}
