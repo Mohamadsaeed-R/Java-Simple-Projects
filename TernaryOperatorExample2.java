0222f4ece6d3531441732d8f534167f315919196
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-19 15:33:15 +0000

    Add files via upload

---
+++ TernaryOperatorExample2.java
@@ -0,0 +1,7 @@
+public class app {
+    public static void main(String[] args) {
+        int AgeOfClient =20;
+        String ageText=AgeOfClient >19?"This client is an adult":"this client is an child";
+        System.out.println(ageText);
+    }
+}
